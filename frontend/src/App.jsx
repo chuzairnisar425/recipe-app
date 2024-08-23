5e916f2e5026a53a1608e2d40c6c7477a343c04f
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-23 10:10:56 +0500

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,20 @@ import Login from "../src/pages/Login";
 import Signup from "../src/pages/Signup";
 import RecipeDetails from "../src/pages/RecipeDetails";
 import Favourites from "../src/pages/Favourites";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
+
       <BrowserRouter>
+      <Navbar/>
+
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<Login />} />
-          <Route path="/login" element={<About />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/About" element={<About />} />
           <Route path="/Signup" element={<Signup />} />
-          <Route path="/RecipeDetails" element={<RecipeDetails />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
           <Route path="/Favourites" element={<Favourites />} />
         </Routes>
       </BrowserRouter>
