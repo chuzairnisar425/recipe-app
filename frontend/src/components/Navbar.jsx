ab59d73a5807009e664431cd365beb5ee9ba68df
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-23 10:22:35 +0500

    frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='flex justify-between bg-white  lg:px-5 md:px-4 py-3 '>
+<Link to='/' className='TITLE-TEXT text-lg text-red-600 font-bold'>HomeChef</Link>
+<div className='flex gap-3 '>
+    <Link to='/About'>About</Link>
+    <Link to='/Login'>Login</Link>
+</div>
+    </nav>
   )
 }
 
