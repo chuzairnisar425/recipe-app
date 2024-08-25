85ee032f8797d78aea3354006ce9ae6a28674cfb
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-25 12:52:17 +0500

    backend

---
+++ frontend/slices/authSlice.js
@@ -0,0 +1,27 @@
+import { createSlice, isAction } from "@reduxjs/toolkit";
+
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        isAuth: false,
+        user: [],
+
+    },
+    reducers: {
+        login: (state) => {
+            state.isAuth= true;
+        },
+        logout:(state)=>{
+            state.isAuth=false
+        },
+        setUser:(state,action)=>{
+            state.user=action.payload;
+        }
+
+
+    }
+})
+
+export const {login, logout,setUser} = authSlice.actions
+export default authSlice.reducer; 
